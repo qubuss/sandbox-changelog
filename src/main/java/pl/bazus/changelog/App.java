c3b9363f4dec0235281953826125fe77a76f3d0e
bogdan.mglowski@bazus.pl <bogdan.mglowski@bazus.pl>
2016-08-11 07:53:27 +0200

    init

---
+++ src/main/java/pl/bazus/changelog/App.java
@@ -0,0 +1,13 @@
+package pl.bazus.changelog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

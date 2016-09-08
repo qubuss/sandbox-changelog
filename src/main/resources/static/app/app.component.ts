5e2104a7d9956953b59adafe4d5c6f2e8924d401
jakub.fryga <JakubFryga@simple.com.pl>
2016-09-08 13:08:16 +0200

    Zmiana 16 #32532 done :)

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Http} from "@angular/http";
-import {IIssues} from "./issue.component"
-import {IBazus} from "./bazus.component"
+import {IIssues} from "./issue.component";
+import {IBazus} from "./bazus.component";
 
 @Component({
     selector: 'my-app',
@@ -14,40 +14,31 @@ export class AppComponent implements OnInit {
     private dataRedmine: IIssues[] = [];
     private dataBazus: IBazus[] = [];
     private pokazDlaIssue = false;
+    private pokazPolaDlaIssue = false;
+    private pokazWersjeBazusa = false;
+    private pokazWszystkieIssue = false;
 
 
     constructor(private http: Http) {
     }
 
     private getAllIssueField(): void {
+        this.hideComponent();
 
-
-        this.pokazDlaIssue = false;
-        this.dataRedmine = [];
-        this.dataBazus = [];
-
-        this.http.get('/getFieldFromAllIssues')
-            .subscribe(
-                data => {
-                    this.dataRedmine = data.json() as IIssues[];
-                    console.info(data);
-                },
-                err => {
-                    console.error('An error occurred', err);
-                    alert('An error occurred!!!');
-                }
-            );
+        if(this.dataRedmine.length==0){
+            this.pobierzDaneZRedmine();
+            console.info("Pobieram dane z Redmine");
+        }
+        this.pokazWszystkieIssue = true;
 
     }
 
     private getIssueField(idIssie: String): void {
-
+        this.pokazDlaIssue = true;
         this.dataRedmine = [];
-        this.dataBazus = [];
-
-        this.http.get('/getFieldFromIssue?idIssue='+idIssie)
+        this.http.get('/getFieldFromIssue?idIssue=' + idIssie)
             .subscribe(
-                data =>{
+                data => {
                     this.dataRedmine.push(data.json() as IIssues);
                     console.info(this.dataRedmine);
                 },
@@ -60,36 +51,89 @@ export class AppComponent implements OnInit {
     }
 
     private getBazusVersion(): void {
+        this.hideComponent();
+
+        if(this.dataBazus.length==0) {
+            this.pobierzWersjeBazusa();
+            console.info("Pobieram wersje Bazusa");
+        }
+
+        this.pokazWersjeBazusa = true;
+    }
+
+    private pobierzWersjeBazusa(): void{
+        this.http.get('/getBazusVersion')
+            .subscribe(
+                data => {
+                    this.dataBazus = data.json() as IBazus[];
+                },
+                err => {
+                    console.error('An error occurred', err);
+                    alert('An error occurred!!!');
+                }
+            );
+    }
+
+
+    private hideComponent(): void {
         this.pokazDlaIssue = false;
+        this.pokazWersjeBazusa = false;
+        this.pokazWszystkieIssue = false;
+        this.pokazPolaDlaIssue = false;
         this.dataRedmine = [];
         this.dataBazus = [];
 
-            this.http.get('/getBazusVersion')
-                .subscribe(
-                    data => {
-                        this.dataBazus = data.json() as IBazus[];
-                    },
-                    err => {
-                        console.error('An error occurred', err);
-                        alert('An error occurred!!!');
-                    }
-                );
     }
 
+    private pokazSzczegolyWersjiBazusa(item): void {
+        item.czyPokazac = !item.czyPokazac;
+        if(item.czyPokazac == true) {
+            this.dataRedmine = [];
+            this.pobierzDaneZRedmineDlaWersjiBazusa(item.idWersja, item.idParent);
+        }else{
+            this.dataRedmine = [];
+        }
+    }
+
+    private showIssueInfo(): void {
+        this.hideComponent();
+        this.pokazPolaDlaIssue = true;
+    }
+
+    private pobierzDaneZRedmine() {
+        this.http.get('/getFieldFromAllIssues')
+            .subscribe(
+                data => {
+                    this.dataRedmine = data.json() as IIssues[];
+
+                    console.info(data);
+                },
+                err => {
+                    console.error('An error occurred', err);
+                    alert('An error occurred!!!');
+                }
+            );
+    }
+
+    private pobierzDaneZRedmineDlaWersjiBazusa(idWersja, idParent) {
+        this.http.get('/getFieldFromAllIssues')
+            .subscribe(
+                data => {
+                    this.dataRedmine = data.json() as IIssues[];
+
+                    console.info(data);
+                },
+                err => {
+                    console.error('An error occurred', err);
+                    alert('An error occurred!!!');
+                }
+            );
+    }
 
     ngOnInit(): void {
-        console.log('Component AppComponent is up and running');
-    }
-
-    private show(): void {
-        this.dataRedmine = [];
-        this.pokazDlaIssue = true;
-        this.dataBazus = [];
+        console.log('Zaczynam prace');
 
     }
-
-
-
 
 }
 
